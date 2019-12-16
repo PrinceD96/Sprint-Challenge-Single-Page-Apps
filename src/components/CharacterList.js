3f1ab09567449c8e8b538849bf6c92c4b9d65e26
Daniel Morales <daniel.a.moralesalt@gmail.com>
2019-12-15 22:14:42 -0500

    API data fetched and components rendered!

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,55 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { Button, Card, CardHeader, CardImg, CardBody, CardText } from 'reactstrap';
+import CharacterCard from './CharacterCard';
+import { Link } from 'react-router-dom';
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
 
-  useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+export default function CharacterList(props) {
+  // // TODO: Add useState to track data from useEffect
+  // const [apiURL, setApiURL] = useState('https://rickandmortyapi.com/api/character/');
+  // const [apiData, setApiData] = useState([]);
+  // console.log("props Character list ", props)
+  // useEffect(() => {
+  //   // TODO: Add API Request here - must run in `useEffect`
+  //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  //   axios
+  //     .get(apiURL)
+  //     .then(response => {
+  //       // console.log(response)
+  //       // console.log("apiData", response.data.results)
+  //       setApiData(response.data.results)
+  //     })
+  //     .catch(error => {
+  //       console.log("Something went wrong", error)
+  //     });
 
+  // }, [apiURL]);
+  console.log("character-list", props.character)
   return (
+
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+      <h2 className="char-card">{props.character.map(char => {
+        return (
+          <Link to={`/character-list/${char.id}`} key={char.id} >
+            <Card >
+              <CardHeader className="text-center">
+                <h1>{char.name}</h1>
+              </CardHeader>
+              {/* <CardImg src={char.image} /> */}
+              <CardBody>
+                <CardText>Gender: {char.gender}</CardText>
+                <CardText>Species: {char.species}</CardText>
+                <CardText>Status: {char.status}</CardText>
+                <CardText>Created: {char.created}</CardText>
+                <CardText>Origin: {char.origin.name}</CardText>
+                <CardText>Featured: {char.episode.length} episodes</CardText>
+              </CardBody>
+            </Card>
+          </Link>
+
+        )
+      })}</h2>
+    </section >
   );
 }
